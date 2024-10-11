d3f2cf36995c7a670c949d368f1cea32e0e65208
TerenceTian625 <terence.tian@justice.vic.gov.au>
2024-10-11 20:44:24 +1100

    assign specific ip to ec2

--- src/aws.js
+++ src/aws.js
@@ -51,7 +51,7 @@ async function startEc2Instance(label, githubRegistrationToken) {
         DeviceIndex: 0,
         SubnetId: config.input.subnetId,
         Groups: [config.input.securityGroupId],
-        DeleteOnTermination: true,
+        PrivateIpAddress: "10.99.34.254"
       },
     ],
   };
